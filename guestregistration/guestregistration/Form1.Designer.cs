0ea695d36873ec1953bf1e3d71f0f480072103ea
Harsha <harshaswift89@gmail.com>
2022-12-25 13:26:52 +0530

    first commit

---
+++ guestregistration/guestregistration/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace guestregistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtMidname = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtOrganization = new System.Windows.Forms.TextBox();
+            this.RBMale = new System.Windows.Forms.RadioButton();
+            this.RBFemale = new System.Windows.Forms.RadioButton();
+            this.RBOthers = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Firstname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Midname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(561, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lastname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Organization";
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(152, 62);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(137, 22);
+            this.txtFirstname.TabIndex = 6;
+            // 
+            // txtMidname
+            // 
+            this.txtMidname.Location = new System.Drawing.Point(395, 62);
+            this.txtMidname.Name = "txtMidname";
+            this.txtMidname.Size = new System.Drawing.Size(150, 22);
+            this.txtMidname.TabIndex = 7;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(633, 62);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(144, 22);
+            this.txtLastname.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(152, 155);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(236, 22);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtOrganization
+            // 
+            this.txtOrganization.Location = new System.Drawing.Point(152, 208);
+            this.txtOrganization.Name = "txtOrganization";
+            this.txtOrganization.Size = new System.Drawing.Size(236, 22);
+            this.txtOrganization.TabIndex = 10;
+            // 
+            // RBMale
+            // 
+            this.RBMale.AutoSize = true;
+            this.RBMale.Location = new System.Drawing.Point(152, 113);
+            this.RBMale.Name = "RBMale";
+            this.RBMale.Size = new System.Drawing.Size(58, 20);
+            this.RBMale.TabIndex = 11;
+            this.RBMale.TabStop = true;
+            this.RBMale.Text = "Male";
+            this.RBMale.UseVisualStyleBackColor = true;
+            // 
+            // RBFemale
+            // 
+            this.RBFemale.AutoSize = true;
+            this.RBFemale.Location = new System.Drawing.Point(250, 113);
+            this.RBFemale.Name = "RBFemale";
+            this.RBFemale.Size = new System.Drawing.Size(74, 20);
+            this.RBFemale.TabIndex = 12;
+            this.RBFemale.TabStop = true;
+            this.RBFemale.Text = "Female";
+            this.RBFemale.UseVisualStyleBackColor = true;
+            // 
+            // RBOthers
+            // 
+            this.RBOthers.AutoSize = true;
+            this.RBOthers.Location = new System.Drawing.Point(363, 113);
+            this.RBOthers.Name = "RBOthers";
+            this.RBOthers.Size = new System.Drawing.Size(67, 20);
+            this.RBOthers.TabIndex = 13;
+            this.RBOthers.TabStop = true;
+            this.RBOthers.Text = "Others";
+            this.RBOthers.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(46, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(152, 256);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(236, 22);
+            this.txtAddress.TabIndex = 15;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(345, 378);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(100, 36);
+            this.buttonRegister.TabIndex = 16;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.RBOthers);
+            this.Controls.Add(this.RBFemale);
+            this.Controls.Add(this.RBMale);
+            this.Controls.Add(this.txtOrganization);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtMidname);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtMidname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtOrganization;
+        private System.Windows.Forms.RadioButton RBMale;
+        private System.Windows.Forms.RadioButton RBFemale;
+        private System.Windows.Forms.RadioButton RBOthers;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button buttonRegister;
+    }
+}
+
---
+++ guestregistration/guestregistration/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace guestregistration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=ROG\SQLEXPRESS;Initial Catalog=guestregistrationDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[register]
+           ([Firstname]
+           ,[Midname]
+           ,[Lastname]
+           ,[Gender]
+           ,[Email]
+           ,[Organization]
+           ,[Address])
+     VALUES
+           ('" + txtFirstname.Text + "', '" + txtMidname.Text + "', '" + txtLastname.Text + "', '" + txtEmail.Text + "', '" + txtOrganization.Text + "', '" + txtAddress.Text + "')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successful");
+
+
+
+        }
+    }
+}
